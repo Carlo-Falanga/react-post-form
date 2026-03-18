86c3d0fe061a878f42ca805db540b71160f54ea4
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-18 13:11:16 +0100

    add boostrap and axios dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from "axios";
+import * as bootstrap from 'bootstrap'
 import './index.css'
 import App from './App.jsx'
 
